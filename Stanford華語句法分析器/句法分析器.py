894f1f8491cc697b146079ae581bfaba1c9b5e50
pigu <a8568730@gmail.com>
2018-07-16 17:53:21 +0800

    安裝mafan轉換繁體華語字做殘體華語字

--- Stanford華語句法分析器/句法分析器.py
+++ Stanford華語句法分析器/句法分析器.py
@@ -1,5 +1,5 @@
 from nltk.parse.corenlp import CoreNLPParser
-
+from mafan import simplify
 
 # 句法分析
 try:
@@ -8,10 +8,9 @@ except Warning as 錯誤:
     print('Warning=', 錯誤)
 
 # 印字串
-#結果 = list(句法分析器.parse("我 喜欢 豬".split()))
-#print('結果=', 結果)
-
+結果 = 句法分析器.parse(simplify("我 喜歡 豬").split())
+next(結果).pretty_print()
 # 印圖表
-next(next(
-    句法分析器.parse("猴子 喜欢 吃 香蕉 。".split())
-)).pretty_print()
+# next(
+#     句法分析器.parse("猴子 喜欢 吃 香蕉 。".split())
+# ).pretty_print()
