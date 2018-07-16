c4bf024a0c5f1f36ceceacc85b5c749fe14dcf15
pigu <a8568730@gmail.com>
2018-07-16 17:07:40 +0800

    執行本機端的StanfordNLP docker server

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+from frnkenstien/corenlp:latest
+COPY ./Stanford華語句法分析器/jars/stanford-chinese-corenlp-2018-02-27-models.jar stanford-corenlp-3.9.1-models.jar
---
+++ Stanford華語句法分析器/句法分析器.py
@@ -0,0 +1,17 @@
+from nltk.parse.corenlp import CoreNLPParser
+
+
+# 句法分析
+try:
+    句法分析器 = CoreNLPParser()
+except Warning as 錯誤:
+    print('Warning=', 錯誤)
+
+# 印字串
+#結果 = list(句法分析器.parse("我 喜欢 豬".split()))
+#print('結果=', 結果)
+
+# 印圖表
+next(
+    句法分析器.parse("猴子 喜欢 吃 香蕉 。".split())
+).pretty_print()
